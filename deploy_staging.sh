1d3250a0fcb8e83dd584b9665b01f60770ee8b1a
Tessa Kriesel <me@tessak22.com>
2017-07-23 18:36:28 -0500

    all together?

--- deploy_staging.sh
+++ deploy_staging.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-ssh tessak22@67.222.18.121 -p 2200
-pause 10 seconds
-$ACCOUNT_PASSWORD
+ssh tessak22@67.222.18.121 -p 2200 pause 10 seconds $ACCOUNT_PASSWORD
 
 echo '1. Updating sources'
 cd www/staging
